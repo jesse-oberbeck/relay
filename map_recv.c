e936b0cbeeef61d25b9cd3a905f6534218277918
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-05-12 16:40:01 -0400

    completely changed approach from sockets to mapped memory.

---
+++ map_recv.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(void)
+{
+    int file;
+    int size;
+    char *data;
+    char *buffer[4096];
+
+    if((file = open("mapped", O_RDONLY, 0644)) == -1){
+        puts("Failed to open file (read).");
+    }
+    size = getpagesize();
+    if((data = mmap(NULL, size, PROT_READ, MAP_SHARED, file, 0)) == -1){
+        perror("mmap");
+    }
+    close(file);
+    for(;;){
+        //printf("%s\n", data);
+        if(strcmp(data, buffer) != 0){
+            strcpy(buffer,data);
+            printf("%s\n", data);
+        }
+        for(int i = 0; data[i] != '\0'; ++i){
+            if(data[i] == 0x04 || data[i] == 0x03){
+                munmap(data, size);
+                exit(0);
+            }
+        }
+    }
+}
---
+++ map_xmit.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <string.h>
+
+
+//http://stackoverflow.com/questions/6698161/getting-raw-input-from-console-using-c-or-c
+/* Initialize new terminal i/o settings */
+static struct termios old, new1;
+void initTermios(int echo) {
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    new1 = old; /* make new settings same as old settings */
+    new1.c_lflag &= ~ICANON; /* disable buffered i/o */
+    new1.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+    tcsetattr(0, TCSANOW, &new1); /* use these new terminal i/o settings now */
+}
+
+
+
+int main(void)
+{
+
+    struct termios term;
+    cfmakeraw(&term);
+
+    int file;
+    int size;
+    char *data;
+
+    initTermios(1);
+
+    file = open("mapped", O_CREAT|O_RDWR|O_TRUNC, 0644);
+    size = 4096;//getpagesize();
+    posix_fallocate(file, 0, size);
+    errno = 0;
+    if((data = mmap(NULL, size, PROT_WRITE, MAP_SHARED, file, 0)) == -1){
+        perror("mmap");
+        exit(1);
+    }
+    close(file);
+    char *c;
+    for(;;){
+
+        c = fgets(data, size, stdin);
+        for(int i = 0; c[i] != NULL; ++i){
+            if(c[i] == 0x04 || c[i] == 0x03){
+                munmap(data, size);
+                data = '\0';
+                exit(0);
+            }
+        }
+
+        //c = getchar();
+        //write(data, c, 1);
+    }
+}
